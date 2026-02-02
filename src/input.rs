5c5c794d5644538f4fe244678694c2ad7e4f796d
errphoenix <herrphoenix20@gmail.com>
2026-02-02 23:45:04 +0100

    Rewrite input system abstractions

--- src/input.rs
+++ src/input.rs
@@ -1,227 +1,252 @@
-use std::{
-    collections::VecDeque,
-    ops::{Deref, DerefMut},
-};
+const KEYBOARD_ENTRIES: usize = 512;
+const MOUSE_ENTRIES: usize = 24;
 
-pub use winit::event::MouseButton as WinitMouseButton;
-pub use winit::keyboard::KeyCode as WinitKeyboardKey;
-
-#[derive(Default, Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct RepeatState {
-    times: i32,
-}
-
-impl RepeatState {
-    fn press(&mut self) {
-        self.times += 1
-    }
-
-    fn release(&mut self) {
-        self.times = -1;
-    }
-
-    fn reset(&mut self) {
-        self.times = 0;
-    }
-
-    pub fn is_pressed(&self) -> bool {
-        self.times == 1
-    }
-
-    pub fn is_released(&self) -> bool {
-        self.times == -1
-    }
-
-    pub fn is_down(&self) -> bool {
-        self.times > 0
-    }
-
-    pub fn is_held(&self) -> bool {
-        self.times > 1
-    }
-}
+const RELEASE_SIGNAL: u16 = 0xFFFF;
+const MAX_HOLD_FRAMES: u16 = 0xFFFF - 1;
 
 #[derive(Debug)]
-pub struct EventInputBuffer<E: Clone + Copy> {
-    buffer: VecDeque<InputState<E>>,
-    frame: usize,
+pub struct Keys {
+    keyboard: [u16; KEYBOARD_ENTRIES],
+    mouse: [u16; MOUSE_ENTRIES],
 }
 
-impl<E: Clone + Copy> Default for EventInputBuffer<E> {
+impl Default for Keys {
     fn default() -> Self {
-        Self {
-            buffer: Default::default(),
-            frame: Default::default(),
-        }
+        Self::new()
     }
 }
 
-impl<E: Clone + Copy> EventInputBuffer<E> {
+impl Keys {
     pub fn new() -> Self {
         Self {
-            buffer: VecDeque::new(),
-            frame: 0,
+            keyboard: [0u16; KEYBOARD_ENTRIES],
+            mouse: [0u16; MOUSE_ENTRIES],
         }
     }
 
-    pub fn release(&mut self) {
-        self.buffer.iter_mut().for_each(|ev| {
-            if ev.is_down() {
-                ev.release();
-            }
-        });
-    }
-
-    pub fn frame(&mut self) {
-        self.frame += 1;
-    }
-
-    pub fn next(&mut self) -> Option<InputState<E>> {
-        let ev = self.buffer.pop_back();
-        if let Some(mut ev) = ev.clone() {
-            if ev.frame > self.frame {
-                // end of current frame events reached
-                return None;
-            }
-
-            if ev.times == 0 {
-                return self.next();
-            }
-
-            if ev.times > 0 {
-                ev.press();
-                self.buffer.push_front(ev.next_frame());
+    pub fn update(&mut self) {
+        for state in self.keyboard.iter_mut() {
+            match *state {
+                RELEASE_SIGNAL => *state = 0,
+                1..=MAX_HOLD_FRAMES => *state += 1,
+                _ => {}
             }
         }
-        ev
+        for state in self.mouse.iter_mut() {
+            match *state {
+                RELEASE_SIGNAL => *state = 0,
+                1..=MAX_HOLD_FRAMES => *state += 1,
+                _ => {}
+            }
+        }
     }
 
-    pub fn clear(&mut self) {
-        self.buffer.clear();
+    pub fn handle_key_events(&mut self, event: &winit::event::WindowEvent) {
+        use winit::event::{ElementState, WindowEvent};
+        use winit::keyboard::PhysicalKey;
+
+        match event {
+            WindowEvent::KeyboardInput { event: key, .. } => {
+                if let PhysicalKey::Code(code) = key.physical_key {
+                    let code = code as usize;
+                    if code < KEYBOARD_ENTRIES {
+                        match key.state {
+                            ElementState::Pressed => {
+                                if self.keyboard[code] == 0 {
+                                    self.keyboard[code] = 1;
+                                }
+                            }
+                            ElementState::Released => {
+                                if self.keyboard[code] > 0 && self.keyboard[code] != RELEASE_SIGNAL
+                                {
+                                    self.keyboard[code] = RELEASE_SIGNAL;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            WindowEvent::MouseInput { state, button, .. } => {
+                let code = {
+                    let button_index: MouseButtonIndex = (*button).into();
+                    usize::from(button_index)
+                };
+
+                match state {
+                    ElementState::Pressed => {
+                        if self.mouse[code] == 0 {
+                            self.mouse[code] = 1;
+                        }
+                    }
+                    ElementState::Released => {
+                        if self.keyboard[code] > 0 && self.keyboard[code] != RELEASE_SIGNAL {
+                            self.keyboard[code] = RELEASE_SIGNAL;
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    pub fn key_frames(&self, code: winit::keyboard::KeyCode) -> u16 {
+        self.keyboard[code as usize]
+    }
+
+    pub fn key_down(&self, code: winit::keyboard::KeyCode) -> bool {
+        self.key_frames(code) != 0
+    }
+
+    pub fn key_pressed(&self, code: winit::keyboard::KeyCode) -> bool {
+        self.key_frames(code) == 1
+    }
+
+    pub fn key_released(&self, code: winit::keyboard::KeyCode) -> bool {
+        self.key_frames(code) == RELEASE_SIGNAL
+    }
+
+    pub fn key_held(&self, code: winit::keyboard::KeyCode) -> bool {
+        self.key_frames(code) > 1
+    }
+
+    pub fn mouse_frames(&self, code: winit::event::MouseButton) -> u16 {
+        let code = {
+            let button_index: MouseButtonIndex = code.into();
+            usize::from(button_index)
+        };
+
+        self.mouse[code]
+    }
+
+    pub fn mouse_down(&self, code: winit::event::MouseButton) -> bool {
+        self.mouse_frames(code) != 0
+    }
+
+    pub fn mouse_pressed(&self, code: winit::event::MouseButton) -> bool {
+        self.mouse_frames(code) == 1
+    }
+
+    pub fn mouse_released(&self, code: winit::event::MouseButton) -> bool {
+        self.mouse_frames(code) == RELEASE_SIGNAL
+    }
+
+    pub fn mouse_held(&self, code: winit::event::MouseButton) -> bool {
+        self.mouse_frames(code) > 1
     }
 }
 
-impl<E: Clone + Copy> Iterator for EventInputBuffer<E> {
-    type Item = InputState<E>;
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct MouseButtonIndex(usize);
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next()
+impl MouseButtonIndex {
+    const LEFT: usize = 0;
+    const RIGHT: usize = 1;
+    const MIDDLE: usize = 2;
+    const BACK: usize = 3;
+    const FORWARD: usize = 4;
+
+    const OTHER_OFFSET: usize = 5;
+}
+
+impl From<MouseButtonIndex> for usize {
+    fn from(value: MouseButtonIndex) -> Self {
+        value.0
     }
 }
 
-#[derive(Debug, Default)]
-pub struct InputBuffer {
-    keyboard: EventInputBuffer<WinitKeyboardKey>,
-    mouse: EventInputBuffer<WinitMouseButton>,
-
-    cursor: (f64, f64),
-    cursor_delta: (f64, f64),
+impl From<winit::event::MouseButton> for MouseButtonIndex {
+    fn from(value: winit::event::MouseButton) -> Self {
+        match value {
+            winit::event::MouseButton::Left => Self(Self::LEFT),
+            winit::event::MouseButton::Right => Self(Self::RIGHT),
+            winit::event::MouseButton::Middle => Self(Self::MIDDLE),
+            winit::event::MouseButton::Back => Self(Self::FORWARD),
+            winit::event::MouseButton::Forward => Self(Self::BACK),
+            winit::event::MouseButton::Other(id) => {
+                Self((id as usize + Self::OTHER_OFFSET).min(MOUSE_ENTRIES - 1))
+            }
+        }
+    }
 }
 
-impl InputBuffer {
+#[derive(Clone, Copy, Debug, Default, PartialEq, PartialOrd)]
+pub struct Cursor {
+    current: (f64, f64),
+    delta: (f64, f64),
+}
+
+impl Cursor {
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn frame(&mut self) {
-        self.keyboard.frame();
-        self.mouse.frame();
+    pub fn update(&mut self) {
+        self.delta = Default::default();
     }
 
-    pub fn clear_all(&mut self) {
-        self.keyboard.clear();
-        self.mouse.clear();
+    pub fn handle_cursor_events(&mut self, event: &winit::event::WindowEvent) {
+        match event {
+            winit::event::WindowEvent::CursorMoved { position, .. } => {
+                self.current = (position.x, position.y);
+            }
+            _ => {}
+        }
     }
 
-    pub fn release_all(&mut self) {
-        self.release_keyboard();
-        self.release_mouse();
+    pub fn handle_cursor_device_events(&mut self, event: &winit::event::DeviceEvent) {
+        match event {
+            winit::event::DeviceEvent::MouseMotion { delta } => {
+                self.delta = *delta;
+            }
+            _ => {}
+        }
     }
 
-    pub fn release_keyboard(&mut self) {
-        self.keyboard.release();
+    pub fn current(&self) -> (f64, f64) {
+        self.current
     }
 
-    pub fn release_mouse(&mut self) {
-        self.mouse.release();
+    pub fn current_f32(&self) -> (f32, f32) {
+        (self.current.0 as f32, self.current.1 as f32)
     }
 
-    pub fn keyboard(&mut self) -> &mut EventInputBuffer<WinitKeyboardKey> {
-        &mut self.keyboard
+    pub fn delta(&self) -> (f64, f64) {
+        self.delta
     }
 
-    pub fn mouse(&mut self) -> &mut EventInputBuffer<WinitMouseButton> {
-        &mut self.mouse
-    }
-
-    pub fn cursor(&self) -> (f64, f64) {
-        self.cursor
-    }
-
-    pub fn cursor_delta(&self) -> (f64, f64) {
-        self.cursor_delta
+    pub fn delta_f32(&self) -> (f32, f32) {
+        (self.delta.0 as f32, self.delta.1 as f32)
     }
 
     pub fn x(&self) -> f64 {
-        self.cursor.0
+        self.current.0
     }
 
     pub fn y(&self) -> f64 {
-        self.cursor.1
+        self.current.1
     }
 
-    pub fn x_delta(&self) -> f64 {
-        self.cursor_delta.0
+    pub fn dx(&self) -> f64 {
+        self.delta.0
     }
 
-    pub fn y_delta(&self) -> f64 {
-        self.cursor_delta.1
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct InputState<T>
-where
-    T: Clone + Copy,
-{
-    frame: usize,
-    key: T,
-    state: RepeatState,
-}
-
-impl<T> InputState<T>
-where
-    T: Clone + Copy,
-{
-    pub fn key(self) -> T {
-        self.key
+    pub fn dy(&self) -> f64 {
+        self.delta.1
     }
 
-    fn next_frame(self) -> Self {
-        Self {
-            frame: self.frame + 1,
-            key: self.key,
-            state: self.state,
-        }
+    pub fn x_f32(&self) -> f32 {
+        self.current.0 as f32
     }
-}
 
-impl<T> Deref for InputState<T>
-where
-    T: Clone + Copy,
-{
-    type Target = RepeatState;
-
-    fn deref(&self) -> &Self::Target {
-        &self.state
+    pub fn y_f32(&self) -> f32 {
+        self.current.1 as f32
     }
-}
 
-impl<T> DerefMut for InputState<T>
-where
-    T: Clone + Copy,
-{
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.state
+    pub fn dx_f32(&self) -> f32 {
+        self.delta.0 as f32
+    }
+
+    pub fn dy_f32(&self) -> f32 {
+        self.delta.1 as f32
     }
 }
